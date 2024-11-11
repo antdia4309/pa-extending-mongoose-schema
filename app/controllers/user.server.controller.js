79b7674850c1edc1fa1ffd8d97fe79f0906e1f86
antdia4309 <antdia4309@students.ecpi.edu>
2024-11-11 12:21:52 -0500

    Updated the user.server.model.js

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -4,10 +4,10 @@ exports.create = async function(req, res){
     const user = new User(req.body);
     await user.save()
     .then((response) => {
-        console.log('Value of response: {0}', response);
+        console.log('Value of response:', response);
         res.status(200).send("User Added");
     })
     .catch((error)=> {
-        console.error('Could not save user: {0}', error);
+        console.error('Could not save user:', error);
     });
 };
